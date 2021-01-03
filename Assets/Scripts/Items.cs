c576f756b55aefe31813723cd35bc772d1fc8315
AsuraX <vpzasurax@gmail.com>
2021-01-03 18:00:56 +0100

    Added Items, Gegner droppt Beutel

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+
+    public GameObject inventory;
+    private bool inRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(inventory.activeInHierarchy && inRange)
+        {
+            inventory.SetActive(false);
+        } else {
+            inventory.SetActive(true);
+        }*/
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+ 
+        if(collider.CompareTag("Player"))
+        {
+            inRange = true;
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+
+        if (collider.CompareTag("Player"))
+        {
+            inRange = false;
+
+        }
+
+    }
+}
