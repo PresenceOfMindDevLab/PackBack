a71fb74bc6c79b7256a412ec27a8822c322df4aa
HartiChan <hartmann.gersdorf@web.de>
2021-01-06 15:38:54 +0100

    unscuffed ver. 0.0.1.1 + dependencies

---
+++ Assets/Scripts/triggerinv.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerinv : MonoBehaviour
+{
+
+    public GameObject fullinventory;
+    public bool inRange;
+    private bool itemsTaken = false;
+    public bool inventoryOpen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player" && itemsTaken == false)
+        {
+            inRange = true;
+            inventoryOpen = true;
+            fullinventory.SetActive(true);
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            inRange = false;
+            itemsTaken = true;
+            inventoryOpen = false;
+            fullinventory.SetActive(false);
+        }
+    }
+}
