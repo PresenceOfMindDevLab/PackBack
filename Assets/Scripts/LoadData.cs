a92a991224f0800b9aa411c51a2e2654797d120f
HartiChan <hartmann.gersdorf@web.de>
2021-01-19 17:24:47 +0100

    add Item load script

---
+++ Assets/Scripts/LoadData.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadData : MonoBehaviour
+{
+
+    public static float[,]  GetItemData()   {
+
+        int i;
+        int len;
+        int ID;
+        float Value;
+        float Weight;
+                
+        GameObject[] Items;
+        Items = GameObject.FindGameObjectsWithTag("Item");
+        len = Items.Length;
+
+        float[,] ItemMatrix = new float[len, 3];
+        i = 0;
+        foreach(GameObject item in Items)  {
+            ID = item.GetComponent<ItemValues>().id;
+            Value = item.GetComponent<ItemValues>().value;
+            Weight = item.GetComponent<ItemValues>().weight;
+
+            ItemMatrix[i, 0] = ID;
+            ItemMatrix[i, 1] = Value;
+            ItemMatrix[i, 2] = Weight;
+
+
+            i += 1;
+        }
+        return ItemMatrix;
+    }
+
+    public void GetItemName(int ID)   {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
