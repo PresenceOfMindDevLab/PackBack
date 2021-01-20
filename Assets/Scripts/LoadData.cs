266f19965a75c935a31d01dde5e94795bb575bd6
HartiChan <hartmann.gersdorf@web.de>
2021-01-20 02:47:13 +0100

    remove Get Game Data

--- Assets/Scripts/LoadData.cs
+++ Assets/Scripts/LoadData.cs
@@ -8,36 +8,6 @@ public class LoadData : MonoBehaviour
 
     Button takeAll;
 
-    public static float[,] GetItemData()
-    {
-
-        string Item;
-        int len;
-        int ID;
-        float Value;
-        float Weight;
-
-        len = 27;
-
-        float[,] ItemMatrix = new float[len, 3];
-
-        for (int i = 1; i < len; i++)
-        {
-            Item = "Item_" + i.ToString();
-            GameObject item = GameObject.Find(Item);
-
-            ID = item.GetComponent<ItemValues>().id;
-            Value = item.GetComponent<ItemValues>().value;
-            Weight = item.GetComponent<ItemValues>().weight;
-            print(ID);
-            ItemMatrix[i, 0] = ID;
-            ItemMatrix[i, 1] = Value;
-            ItemMatrix[i, 2] = Weight;
-
-        }
-        return ItemMatrix;
-    }
-
     public static string GetItemName(int ID)   {
         string Item;
         string name;
